695e65c2671b9d64f39c1847506ce419d026d3dd
chyoang30 <chyoang30@gmail.com>
2025-05-20 20:15:45 +0900

    fix: comment on

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def generate_sentence():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)   # 로컬 테스트용
+    # app.run(debug=True)   # 로컬 테스트용
     
-    # port = int(os.environ.get('PORT', 5000))  # 배포용
-    # app.run(debug=False, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))  # 배포용
+    app.run(debug=False, host='0.0.0.0', port=port)
