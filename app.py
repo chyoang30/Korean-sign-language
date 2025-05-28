0127709cf32795bdec402aafff7c7c57a2cfab6b
chyoang30 <chyoang30@gmail.com>
2025-05-28 15:38:28 +0900

    final presentation

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def upload_proxy():
 
     try:
         # ngrok 주소 사용
-        resp = requests.post("https://60d6-218-150-183-121.ngrok-free.app/upload", files=files)
+        resp = requests.post("https://fd50-203-255-221-65.ngrok-free.app/upload", files=files)
         return jsonify(resp.json())
     except Exception as e:
         return jsonify({"error": f"로컬 추론 서버 호출 실패: {e}"}), 500
